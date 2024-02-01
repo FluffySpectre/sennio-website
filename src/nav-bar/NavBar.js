b0fa9fda903709786710be22e3378d26d2cce8bf
Björn Bosse <b.bosse1991@gmail.com>
2024-02-01 22:03:51 +0100

    Refactored nav-bar and extracted nav-link stuff into separate component

--- src/nav-bar/NavBar.js
+++ src/nav-bar/NavBar.js
@@ -1,30 +1,27 @@
 import { withTranslation } from "react-i18next";
 import "./NavBar.css";
+import NavLink from "../nav-link/NavLink";
 
 function NavBar(props) {
-  const t = props.t;
-
   // nav links
   const navLinks = [
     { page: "projects", title: "menuLinks.projects" },
+    // { page: "skills", title: "menuLinks.skills" },
     // { page: "about-me", title: "menuLinks.aboutMe" },
     // { page: "contact", title: "menuLinks.contact" },
   ];
 
   const navLinkElements = navLinks.map((n, i) => (
     <span key={"NavLink" + i}>
-      <span className="nav-link" onClick={() => props.navTo(n.page)}>
-        <span
-          style={{
-            visibility: props.page === n.page ? "visible" : "hidden",
-          }}
-        >
-          &gt;&nbsp;
-        </span>
-        {t(n.title)}
-        <span style={{ visibility: "hidden" }}>&gt;&nbsp;</span>
-      </span>
-      {/* {i < navLinks.length - 1 && <span className="separator">&#x2022;</span>} */}
+      <NavLink
+        targetPage={n.page}
+        title={n.title}
+        page={props.page}
+        navTo={props.navTo}
+      />
+      {/* {navLinks.length > 1 && i < navLinks.length - 1 && (
+        <span className="separator">&#x2022;</span>
+      )} */}
     </span>
   ));
 
---
+++ src/nav-link/NavLink.js
@@ -0,0 +1,33 @@
+import { withTranslation } from "react-i18next";
+import "./NavLink.css";
+
+function NavLink(props) {
+  const t = props.t;
+
+  return (
+    <span
+      className="NavLink"
+      style={props.style}
+      onClick={() => props.navTo(props.targetPage)}
+    >
+      <span
+        style={{
+          visibility: props.page === props.targetPage ? "visible" : "hidden",
+        }}
+      >
+        &lt;&nbsp;
+      </span>
+      {t(props.title)}
+      <span
+        style={{
+          visibility: props.page === props.targetPage ? "visible" : "hidden",
+        }}
+      >
+        &nbsp;/&gt;
+      </span>
+      <span style={{ visibility: "hidden" }}>&nbsp;/&gt;</span>
+    </span>
+  );
+}
+
+export default withTranslation()(NavLink);
