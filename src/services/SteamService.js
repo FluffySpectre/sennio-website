3d61f90ce03c9f41967905c5f0e8223a8f40b07b
Björn Bosse <b.bosse1991@gmail.com>
2024-02-08 22:25:38 +0100

    Added redirect to index.php (front controller)

--- src/services/SteamService.js
+++ src/services/SteamService.js
@@ -1,7 +1,7 @@
 class SteamService {
   async getLastPlayedGames() {
     // const backendAPI = "http://localhost/website-backend/index.php";
-    const backendAPI = "https://sennio.de/website-backend/index.php";
+    const backendAPI = "https://sennio.de/website-backend";
     const response = await fetch(backendAPI);
     const data = await response.json();
     return data.games;
