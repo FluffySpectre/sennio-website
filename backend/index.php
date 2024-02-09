69a4aa5be88439f913b3a90f421d54296bcd0dfa
Björn Bosse <b.bosse1991@gmail.com>
2024-02-09 17:22:08 +0100

    Backend: Added a blacklist filter

--- backend/index.php
+++ backend/index.php
@@ -16,6 +16,16 @@ if (!isset($lastPlayedGames)) {
     $lastPlayedGames = [];
 }
 
+// apply blacklist filter
+$blacklistFile = __DIR__ . "/blacklist.json";
+if (file_exists($blacklistFile)) {
+    $blacklistData = json_decode(file_get_contents($blacklistFile), true);
+    $blacklist = $blacklistData["blacklist"];
+    $lastPlayedGames = array_filter($lastPlayedGames, function ($g) use ($blacklist) {
+        return array_search($g["appID"], $blacklist) === false;
+    });
+}
+
 // limit the array to $maxGames
 if (count($lastPlayedGames) > $maxGames) {
     array_splice($lastPlayedGames, $maxGames);
