86a1b698385107b54303bdadd3263d8eec559141
Björn Bosse <b.bosse1991@gmail.com>
2024-02-09 17:53:21 +0100

    Added caching to backend service

--- src/services/SteamService.js
+++ src/services/SteamService.js
@@ -1,8 +1,10 @@
+import reqmate from "reqmate";
+
 class SteamService {
   async getLastPlayedGames() {
     // const backendAPI = "http://localhost/website-backend/index.php";
     const backendAPI = "https://sennio.de/website-backend";
-    const response = await fetch(backendAPI);
+    const response = await reqmate.get(backendAPI).setCaching(60000).send();
     const data = await response.json();
     return data.games;
   }
