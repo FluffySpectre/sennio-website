ce401423270f9ac6bf14c4a55e8b25a5f9f72580
Björn Bosse <b.bosse1991@gmail.com>
2024-02-09 18:00:41 +0100

    Fixed backend url

--- src/services/SteamService.js
+++ src/services/SteamService.js
@@ -2,8 +2,8 @@ import reqmate from "reqmate";
 
 class SteamService {
   async getLastPlayedGames() {
-    const backendAPI = "http://localhost/website-backend/index.php";
-    // const backendAPI = "https://sennio.de/website-backend";
+    // const backendAPI = "http://localhost/website-backend/index.php";
+    const backendAPI = "https://sennio.de/website-backend";
     const response = await reqmate.get(backendAPI).setCaching(60000).send();
     const data = await response.data;
     return data.games;
