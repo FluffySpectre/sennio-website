db8aeeb53504d361cf99558b5756768080cb2b97
Björn Bosse <b.bosse1991@gmail.com>
2024-02-10 10:50:18 +0100

    Backend: Refactored routing

--- backend/index.php
+++ backend/index.php
@@ -1,36 +1,57 @@
 <?php
 
-// max games in list
-$maxGames = 18;
+// routing
+get("/website-backend/last-played-games", "getLastPlayedGames");
 
-// Load save file infos
-$saveFile = __DIR__ . "/save.json";
-if (file_exists($saveFile)) {
-    $saveFileContent = json_decode(file_get_contents($saveFile), true);
-    $lastPlayedGames = $saveFileContent["lastPlayedGames"];
-    $lastUpdateTimestamp = $saveFileContent["lastUpdateTimestamp"];
+
+
+// route handler
+function getLastPlayedGames() {
+    // max games in list
+    $maxGames = 18;
+
+    // load save file infos
+    $saveFile = __DIR__ . "/save.json";
+    if (file_exists($saveFile)) {
+        $saveFileContent = json_decode(file_get_contents($saveFile), true);
+        $lastPlayedGames = $saveFileContent["lastPlayedGames"];
+    }
+
+    // check if an update is needed
+    if (!isset($lastPlayedGames)) {
+        $lastPlayedGames = [];
+    }
+
+    // apply blacklist filter
+    $blacklistFile = __DIR__ . "/blacklist.json";
+    if (file_exists($blacklistFile)) {
+        $blacklistData = json_decode(file_get_contents($blacklistFile), true);
+        $blacklist = $blacklistData["blacklist"];
+        $lastPlayedGames = array_filter($lastPlayedGames, function ($g) use ($blacklist) {
+            return array_search($g["appID"], $blacklist) === false;
+        });
+    }
+
+    // limit the array to $maxGames
+    if (count($lastPlayedGames) > $maxGames) {
+        array_splice($lastPlayedGames, $maxGames);
+    }
+
+    // return the response in JSON format
+    jsonResponse(["games" => array_values($lastPlayedGames)]);
 }
 
-// Check if an update is needed
-if (!isset($lastPlayedGames)) {
-    $lastPlayedGames = [];
+// functions
+function get($route, $callback) {
+    $reqMethod = $_SERVER["REQUEST_METHOD"];
+    $reqURI = $_SERVER["REQUEST_URI"];
+
+    if ($reqMethod === "GET" && $reqURI === $route) {
+        call_user_func($callback);
+    }
 }
 
-// apply blacklist filter
-$blacklistFile = __DIR__ . "/blacklist.json";
-if (file_exists($blacklistFile)) {
-    $blacklistData = json_decode(file_get_contents($blacklistFile), true);
-    $blacklist = $blacklistData["blacklist"];
-    $lastPlayedGames = array_filter($lastPlayedGames, function ($g) use ($blacklist) {
-        return array_search($g["appID"], $blacklist) === false;
-    });
+function jsonResponse($data) {
+    header("Content-Type: application/json");
+    echo json_encode($data);
 }
-
-// limit the array to $maxGames
-if (count($lastPlayedGames) > $maxGames) {
-    array_splice($lastPlayedGames, $maxGames);
-}
-
-// Return the response in JSON format
-header("Content-Type: application/json");
-echo json_encode(["games" => array_values($lastPlayedGames)]);
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import cachedJSONFetch from "../cached-fetch";
 
 export const getLastPlayedGames = async () => {
-  // const backendAPI = "http://localhost/website-backend/index.php";
-  const backendAPI = "https://sennio.de/website-backend";
+  // const backendAPI = "http://localhost/website-backend/last-played-games";
+  const backendAPI = "https://sennio.de/website-backend/last-played-games";
   const response = await cachedJSONFetch(backendAPI, 60000);
   return response.games;
 };
