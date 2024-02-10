cd91b8d53c53ea0d41209e9c2babc9e1618e2271
Björn Bosse <b.bosse1991@gmail.com>
2024-02-10 13:27:45 +0100

    Refactored backend api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,12 @@
 import cachedJSONFetch from "../cached-fetch";
 
+// const backendAPI = "http://localhost/website-backend";
+const backendAPI = "https://sennio.de/website-backend";
+
 export const getLastPlayedGames = async () => {
-  // const backendAPI = "http://localhost/website-backend/last-played-games";
-  const backendAPI = "https://sennio.de/website-backend/last-played-games";
-  const response = await cachedJSONFetch(backendAPI, 60000);
+  const response = await cachedJSONFetch(
+    backendAPI + "/last-played-games",
+    60000
+  );
   return response.games;
 };
