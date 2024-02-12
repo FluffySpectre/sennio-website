646da962182c315b2a2197592bccf21f5635eb9f
Björn Bosse <b.bosse1991@gmail.com>
2024-02-12 22:31:58 +0100

    Increased fetch timeout to 2 seconds

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,6 @@ import retryFetch from "../utils/retry-fetch";
 const backendAPI = "https://sennio.de/website-backend";
 
 export const getLastPlayedGames = async () => {
-  const response = await retryFetch(backendAPI + "/last-played-games", 3, 1000);
+  const response = await retryFetch(backendAPI + "/last-played-games", 3, 2000);
   return response.games;
 };
