1346984489dc83bdcd0febf529d47b1e8ad78310
Björn Bosse <b.bosse1991@gmail.com>
2024-02-12 18:31:14 +0100

    Fixed api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import retryFetch from "../retry-fetch";
 
-const backendAPI = "http://localhost/website-backend";
-// const backendAPI = "https://sennio.de/website-backend";
+// const backendAPI = "http://localhost/website-backend";
+const backendAPI = "https://sennio.de/website-backend";
 
 export const getLastPlayedGames = async () => {
   const response = await retryFetch(backendAPI + "/last-played-games", 3, 1000);
