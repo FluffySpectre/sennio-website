f2922c2dfbc27d3e8f4430041e63c4c9dd212f96
Björn Bosse <b.bosse1991@gmail.com>
2024-02-22 21:00:30 +0100

    Added a curve to change the delay between retries with each fetch attempt

--- src/utils/retry-fetch.js
+++ src/utils/retry-fetch.js
@@ -1,7 +1,17 @@
+const curveLinear = (attempt) => attempt;
+
+const curves = {
+  linear: curveLinear,
+};
+
 const withRetry = (fetchFunction) => {
   return async (url, opts = {}) => {
     const maxRetries = opts.maxRetries || 3;
     const msBetweenRetries = opts.msBetweenRetries || 500;
+    let retryDelayCurve = opts.retryDelayCurve || curves.linear;
+    if (typeof retryDelayCurve === "string") {
+      retryDelayCurve = curves[retryDelayCurve];
+    }
 
     for (let attempt = 1; attempt <= maxRetries; attempt++) {
       try {
@@ -10,7 +20,10 @@ const withRetry = (fetchFunction) => {
         console.log(`Attempt ${attempt}: ${error.message}`);
         if (attempt === maxRetries)
           throw new Error(`Max retries reached. ${error.message}`);
-        await new Promise((resolve) => setTimeout(resolve, msBetweenRetries));
+
+        const retryDelay = msBetweenRetries * retryDelayCurve(attempt);
+        console.log(`Retrying in ${retryDelay}ms`);
+        await new Promise((resolve) => setTimeout(resolve, retryDelay));
       }
     }
   };
